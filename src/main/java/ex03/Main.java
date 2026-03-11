79889a103c05d8f868bcbfbcc4ffbf454e5d894d
glebboxik-cmyk <glebboxik@gmail.com>
2026-03-11 18:02:10 +0200

    Implement main method to initialize and show view

--- src/main/java/ex03/Main.java
+++ src/main/java/ex03/Main.java
@@ -1,4 +1,11 @@
 package ex03;
 
+import ex02.View;
+
 public class Main {
+    public static void main(String[] args) {
+        View v = new ViewableTable().getView();
+        v.viewInit();
+        v.viewShow();
+    }
 }
