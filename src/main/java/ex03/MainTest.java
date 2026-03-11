eec000f22fff3cf9647758fb8bf9ded6c60f5c05
glebboxik-cmyk <glebboxik@gmail.com>
2026-03-11 18:02:33 +0200

    Add main method to MainTest for testing ViewTable

--- src/main/java/ex03/MainTest.java
+++ src/main/java/ex03/MainTest.java
@@ -1,4 +1,14 @@
 package ex03;
 
 public class MainTest {
+    public static void main(String[] args) {
+        ViewTable vt = new ViewTable();
+        vt.init(35);
+
+        if (vt.getItems().size() > 0) {
+            System.out.println("Test tablytsi: ok");
+        } else {
+            System.out.println("Test tablytsi: pomylka");
+        }
+    }
 }
